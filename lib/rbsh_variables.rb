4a9a751fe46457571a5c5f200c1d05a4483beaae
Jared Branum <jared.branum@gmail.com>
2011-03-02 17:29:30 -0500

    removed RbshVariables.running variable

--- lib/rbsh_variables.rb
+++ lib/rbsh_variables.rb
@@ -1,13 +1,7 @@
 class RbshVariables
   class << self;
-    attr_accessor :command
     attr_accessor :context
-    attr_writer   :running
     attr_writer   :system_command
-
-    def running?
-      !!@running
-    end
 
     def system_command?
       !!@system_command
